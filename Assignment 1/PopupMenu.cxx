e094b792fe2221865d88403b28d3103973f70ca7
James Thorne <james@dtscc.net>
2012-09-18 13:03:29 -0600

    Added comments

--- Assignment 1/PopupMenu.cxx
+++ Assignment 1/PopupMenu.cxx
@@ -19,12 +19,12 @@ int glutNextItemId_ = 0;
 PopupMenu* activeMenu = NULL;
 void glutHandleMenu(int);
 
-
 PopupMenu::PopupMenu(I_PopupMenuHandler& handler, int x, int y, int width)
    : handler_(handler)
    , x_(x)
    , y_(y)
 {
+   // Create the GLUT menu
    glutId_ = glutCreateMenu(glutHandleMenu);
 }
 
@@ -35,33 +35,46 @@ PopupMenu::~PopupMenu()
 /**
  ******************************************************************************
  *
- *                   Setup & Visiblity
+ *                   Control which menu is active
  *
  ******************************************************************************
  */
-
 void PopupMenu::setAsActiveMenu()
 {
+   // Tell GLUT we're the active menu
    glutSetMenu(glutId_);
    glutAttachMenu(GLUT_LEFT_BUTTON);
 
+   // Store ourself as the active menu for our handler function
    activeMenu = this;
 }
 
 void PopupMenu::deactivateMenus()
 {
+   // Deactivate menus in GLUT
    glutDetachMenu(GLUT_LEFT_BUTTON);
-   
+
+   // Ensure the handler doesn't call a random menu
    activeMenu = NULL;
 }
 
+/**
+ ******************************************************************************
+ *
+ *                   Add an item to this menu
+ *
+ ******************************************************************************
+ */
 void PopupMenu::addItem(std::string item)
 {
+   // Add to the item list
    items_.push_back(item);
 
+   // Determine what ID we'll give to GLUT
    int glutId = glutNextItemId_++;
    glutIdToItemIndex_[glutId] = items_.size() - 1;
 
+   // Setup the menu in GLUT
    glutSetMenu(glutId_);
    glutAddMenuEntry(item.data(), glutId);
 }
@@ -69,33 +82,35 @@ void PopupMenu::addItem(std::string item)
 /**
  ******************************************************************************
  *
- *                   Menu Item Clicking
+ *                   Handle the menu being clicked
  *
  ******************************************************************************
  */
 void PopupMenu::handleMenuItemSelected(int glutId)
 {
+   // Figure out which item was clicked
    int index = glutIdToItemIndex_[glutId];
    std::string name = items_[index];
 
+   // Tell our handler_ that it was selected
    handler_.handleItemSelected(this, index, name);
 }
 
 /**
  ******************************************************************************
  *
- *                   GLUT Menu Handling
+ *                   Low-level GLUT menu handelr
  *
  ******************************************************************************
  */
 void glutHandleMenu(int itemId)
 {
+   // Ensure a menu was really active
    if (activeMenu == NULL)
    {
       return;
    }
 
+   // Tell the menu it was clicked
    activeMenu->handleMenuItemSelected(itemId);
 }
-
-
